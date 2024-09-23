17a0034501f058a71fff04b4ee372cd87633eb58
Mike-Brain <Michael.brain1@justice.gov.uk>
2024-09-23 13:44:50 +0100

    RET-3446, RET-3447 & RET-3448 - Respondent Employees, Sites and Employees at the site.
    * added radio buttons for Respondent Sites

--- src/main/controllers/RespondentSitesController.ts
+++ src/main/controllers/RespondentSitesController.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 
 import { Form } from '../components/form';
 import { AppRequest } from '../definitions/appRequest';
+import { YesOrNoOrNotSure } from '../definitions/case';
 import { PageUrls, TranslationKeys } from '../definitions/constants';
 import { FormContent, FormFields } from '../definitions/form';
 import { saveForLaterButton, submitButton } from '../definitions/radios';
@@ -10,6 +11,7 @@ import { postLogic } from '../helpers/CaseHelpers';
 import { assignFormData, getPageContent } from '../helpers/FormHelper';
 import { setUrlLanguage } from '../helpers/LanguageHelper';
 import { getLogger } from '../logger';
+import { isOptionSelected } from '../validators/validator';
 
 const logger = getLogger('RespondentSitesController');
 
@@ -23,6 +25,24 @@ export default class RespondentSitesController {
         type: 'text',
         hint: (l: AnyRecord): string => l.hint,
         labelHidden: true,
+        values: [
+          {
+            name: 'respondentSites',
+            label: (l: AnyRecord): string => l.yes,
+            value: YesOrNoOrNotSure.YES,
+          },
+          {
+            name: 'respondentSites',
+            label: (l: AnyRecord): string => l.no,
+            value: YesOrNoOrNotSure.NO,
+          },
+          {
+            name: 'respondentSites',
+            label: (l: AnyRecord): string => l.notSure,
+            value: YesOrNoOrNotSure.NOT_SURE,
+          },
+        ],
+        validator: isOptionSelected,
       },
     },
     submit: submitButton,
