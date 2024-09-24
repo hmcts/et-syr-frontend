bb4e4f990cd0980fa027b9e1129b612c88bceacf
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2024-09-24 14:10:06 +0100

    Create AcasEarlyConciliationCertificateController

---
+++ src/main/controllers/AcasEarlyConciliationCertificateController.ts
@@ -0,0 +1,72 @@
+import { Response } from 'express';
+
+import { Form } from '../components/form';
+import { AppRequest } from '../definitions/appRequest';
+import { YesOrNo } from '../definitions/case';
+import { PageUrls, TranslationKeys } from '../definitions/constants';
+import { FormContent, FormFields } from '../definitions/form';
+import { saveForLaterButton, submitButton } from '../definitions/radios';
+import { AnyRecord } from '../definitions/util-types';
+import { postLogic } from '../helpers/CaseHelpers';
+import { assignFormData, getPageContent } from '../helpers/FormHelper';
+import { getLogger } from '../logger';
+import { isContent2500CharsOrLess, isOptionSelected } from '../validators/validator';
+
+const logger = getLogger('AcasEarlyConciliationCertificateController');
+
+export default class AcasEarlyConciliationCertificateController {
+  form: Form;
+  private readonly formContent: FormContent = {
+    fields: {
+      disagreeEarlyConciliation: {
+        type: 'radios',
+        label: (l: AnyRecord): string => l.label,
+        values: [
+          {
+            label: (l: AnyRecord): string => l.no,
+            value: YesOrNo.NO,
+          },
+          {
+            label: (l: AnyRecord): string => l.yes,
+            value: YesOrNo.YES,
+            subFields: {
+              disagreeEarlyConciliationWhy: {
+                type: 'textarea',
+                id: 'disagreeEarlyConciliationWhy',
+                label: (l: AnyRecord): string => l.whyLabel,
+                attributes: {
+                  maxLength: 2500,
+                },
+                validator: isContent2500CharsOrLess,
+              },
+            },
+          },
+        ],
+        validator: isOptionSelected,
+      },
+    },
+    submit: submitButton,
+    saveForLater: saveForLaterButton,
+  } as never;
+
+  constructor() {
+    this.form = new Form(<FormFields>this.formContent.fields);
+  }
+
+  public post = async (req: AppRequest, res: Response): Promise<void> => {
+    await postLogic(req, res, this.form, logger, PageUrls.CLAIMANT_EMPLOYMENT_DATES);
+  };
+
+  public get = (req: AppRequest, res: Response): void => {
+    const content = getPageContent(req, this.formContent, [
+      TranslationKeys.COMMON,
+      TranslationKeys.ACAS_EARLY_CONCILIATION_CERTIFICATE,
+      TranslationKeys.SIDEBAR_CONTACT_US,
+    ]);
+    assignFormData(req.session.userCase, this.form.getFormFields());
+    res.render(TranslationKeys.ACAS_EARLY_CONCILIATION_CERTIFICATE, {
+      ...content,
+      hideContactUs: true,
+    });
+  };
+}
---
+++ src/test/unit/controllers/ClaimantJobTitleController.test.ts
@@ -0,0 +1,85 @@
+import ClaimantJobTitleController from '../../../main/controllers/ClaimantJobTitleController';
+import { YesOrNoOrNotSure } from '../../../main/definitions/case';
+import { PageUrls, TranslationKeys, languages } from '../../../main/definitions/constants';
+import pageJsonRaw from '../../../main/resources/locales/en/translation/acas-early-conciliation-certificate.json';
+import commonJsonRaw from '../../../main/resources/locales/en/translation/common.json';
+import { mockRequest, mockRequestWithTranslation } from '../mocks/mockRequest';
+import { mockResponse } from '../mocks/mockResponse';
+
+describe('Claimant job title Controller', () => {
+  const translationJsons = { ...pageJsonRaw, ...commonJsonRaw };
+  let controller: ClaimantJobTitleController;
+  let request: ReturnType<typeof mockRequest>;
+  let response: ReturnType<typeof mockResponse>;
+
+  beforeEach(() => {
+    controller = new ClaimantJobTitleController();
+    request = mockRequest({});
+    response = mockResponse();
+  });
+
+  describe('GET method', () => {
+    it('should render the page', () => {
+      request = mockRequestWithTranslation({}, translationJsons);
+      controller.get(request, response);
+
+      expect(response.render).toHaveBeenCalledWith(TranslationKeys.CLAIMANT_JOB_TITLE, expect.anything());
+    });
+  });
+
+  describe('POST method', () => {
+    it('should redirect to next page when yes is selected', () => {
+      request = mockRequest({
+        body: {
+          isClaimantJobTitleCorrect: YesOrNoOrNotSure.YES,
+        },
+      });
+      request.url = PageUrls.CLAIMANT_JOB_TITLE + languages.ENGLISH_URL_PARAMETER;
+      controller.post(request, response);
+
+      expect(response.redirect).toHaveBeenCalledWith(
+        PageUrls.CLAIMANT_AVERAGE_WEEKLY_WORK_HOURS + languages.ENGLISH_URL_PARAMETER
+      );
+    });
+
+    it('should redirect to next page when no is selected', () => {
+      request = mockRequest({
+        body: {
+          isClaimantJobTitleCorrect: YesOrNoOrNotSure.NO,
+          whatIsClaimantJobTitle: 'Test',
+        },
+      });
+      request.url = PageUrls.CLAIMANT_JOB_TITLE + languages.ENGLISH_URL_PARAMETER;
+      controller.post(request, response);
+
+      expect(response.redirect).toHaveBeenCalledWith(
+        PageUrls.CLAIMANT_AVERAGE_WEEKLY_WORK_HOURS + languages.ENGLISH_URL_PARAMETER
+      );
+    });
+
+    it('should redirect to next page when Not Sure is selected', () => {
+      request = mockRequest({
+        body: {
+          isClaimantJobTitleCorrect: YesOrNoOrNotSure.NOT_SURE,
+        },
+      });
+      request.url = PageUrls.CLAIMANT_JOB_TITLE + languages.ENGLISH_URL_PARAMETER;
+      controller.post(request, response);
+
+      expect(response.redirect).toHaveBeenCalledWith(
+        PageUrls.CLAIMANT_AVERAGE_WEEKLY_WORK_HOURS + languages.ENGLISH_URL_PARAMETER
+      );
+    });
+
+    it('should render the same page when nothing is selected', () => {
+      request = mockRequest({ body: {} });
+      request.url = PageUrls.CLAIMANT_JOB_TITLE + languages.ENGLISH_URL_PARAMETER;
+      controller.post(request, response);
+
+      expect(response.redirect).toHaveBeenCalledWith(PageUrls.CLAIMANT_JOB_TITLE + languages.ENGLISH_URL_PARAMETER);
+
+      const errors = [{ propertyName: 'isClaimantJobTitleCorrect', errorType: 'required' }];
+      expect(request.session.errors).toEqual(errors);
+    });
+  });
+});
