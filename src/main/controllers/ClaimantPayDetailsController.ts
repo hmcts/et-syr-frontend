b7e9f900556a8770cc6b9460cd17df73eda7d7e0
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2024-09-25 16:21:13 +0100

    Add YesNoNotSureRadio

--- src/main/controllers/ClaimantPayDetailsController.ts
+++ src/main/controllers/ClaimantPayDetailsController.ts
@@ -5,12 +5,10 @@ import { AppRequest } from '../definitions/appRequest';
 import { YesOrNoOrNotSure } from '../definitions/case';
 import { PageUrls, TranslationKeys } from '../definitions/constants';
 import { FormContent, FormFields } from '../definitions/form';
-import { saveForLaterButton, submitButton } from '../definitions/radios';
-import { AnyRecord } from '../definitions/util-types';
+import { YesNoNotSureRadio, saveForLaterButton, submitButton } from '../definitions/radios';
 import { postLogic } from '../helpers/CaseHelpers';
 import { assignFormData, getPageContent } from '../helpers/FormHelper';
 import { getLogger } from '../logger';
-import { isOptionSelected } from '../validators/validator';
 
 const logger = getLogger('ClaimantPayDetailsController');
 
@@ -18,27 +16,7 @@ export default class ClaimantPayDetailsController {
   form: Form;
   private readonly formContent: FormContent = {
     fields: {
-      arePayDetailsGivenCorrect: {
-        type: 'radios',
-        label: (l: AnyRecord): string => l.label,
-        values: [
-          {
-            label: (l: AnyRecord): string => l.yes,
-            value: YesOrNoOrNotSure.YES,
-          },
-          {
-            label: (l: AnyRecord): string => l.no,
-            value: YesOrNoOrNotSure.NO,
-            hint: (l: AnyRecord): string => l.noHintLabel,
-          },
-          {
-            label: (l: AnyRecord): string => l.notSure,
-            value: YesOrNoOrNotSure.NOT_SURE,
-            hint: (l: AnyRecord): string => l.notSureHintLabel,
-          },
-        ],
-        validator: isOptionSelected,
-      },
+      arePayDetailsGivenCorrect: YesNoNotSureRadio,
     },
     submit: submitButton,
     saveForLater: saveForLaterButton,
