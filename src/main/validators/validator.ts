508b0937b596ba2dfafb97728bb8f76221ffb0ab
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2024-10-08 12:01:53 +0100

    RET-3468 (#236)

--- src/main/validators/validator.ts
+++ src/main/validators/validator.ts
@@ -72,19 +72,6 @@ export const isValidUKTelNumber: Validator = value => {
   }
 };
 
-export const isJobTitleValid: Validator = value => {
-  if (typeof value === 'string') {
-    const inputStrLength = (value as string).trim().length;
-    if (inputStrLength === 0) {
-      return;
-    }
-
-    if (inputStrLength === 1 || inputStrLength > 100) {
-      return ValidationErrors.INVALID_LENGTH;
-    }
-  }
-};
-
 export const isValidTwoDigitInteger: Validator = value => {
   if (!value || (value as string).trim().length === 0) {
     return ValidationErrors.INVALID_VALUE;
