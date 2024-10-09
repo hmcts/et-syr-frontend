c9b5b067eaa63f7bdfda9088d7beb748ee2c393c
mehmet.dede <mehmet.dede@justice.gov.uk>
2024-10-09 16:45:07 +0100

    Added update service to RespondentNameController.

--- src/test/unit/controllers/CheckYourAnswersEmployersContractClaimController.test.ts
+++ src/test/unit/controllers/CheckYourAnswersEmployersContractClaimController.test.ts
@@ -1,11 +1,12 @@
 import CheckYourAnswersEmployersContractClaimController from '../../../main/controllers/CheckYourAnswersEmployersContractClaimController';
 import { PageUrls, TranslationKeys } from '../../../main/definitions/constants';
-import { FormError } from '../../../main/definitions/form';
 import pageJsonRaw from '../../../main/resources/locales/cy/translation/check-your-answers-et3-common.json';
 import commonJsonRaw from '../../../main/resources/locales/cy/translation/common.json';
 import ET3Util from '../../../main/utils/ET3Util';
+import { mockCaseWithIdWithRespondents } from '../mocks/mockCaseWithId';
 import { mockRequest, mockRequestWithTranslation } from '../mocks/mockRequest';
 import { mockResponse } from '../mocks/mockResponse';
+import { createMockedUpdateET3ResponseWithET3FormFunction, mockFormError } from '../mocks/mockStaticFunctions';
 
 jest.mock('../../../main/helpers/controller/CheckYourAnswersET3Helper');
 jest.mock('../../../main/utils/ET3Util');
@@ -14,19 +15,20 @@ describe('CheckYourAnswersEmployersContractClaimController', () => {
   let controller: CheckYourAnswersEmployersContractClaimController;
   let request: ReturnType<typeof mockRequest>;
   let response: ReturnType<typeof mockResponse>;
-  const userCase = {}; // mock userCase object as needed
   const translationJsons = { ...pageJsonRaw, ...commonJsonRaw };
+  const updateET3ResponseWithET3FormMock = jest.fn();
 
   beforeEach(() => {
     controller = new CheckYourAnswersEmployersContractClaimController();
     request = mockRequest({
       session: {
-        userCase,
+        mockCaseWithIdWithRespondents,
         selectedRespondent: {
           employersContractClaimSection: 'Yes',
         },
       },
     });
+    ET3Util.updateET3ResponseWithET3Form = updateET3ResponseWithET3FormMock;
     response = mockResponse();
     jest.clearAllMocks();
   });
@@ -35,11 +37,6 @@ describe('CheckYourAnswersEmployersContractClaimController', () => {
     it('should render the page', () => {
       request = mockRequestWithTranslation({}, translationJsons);
       controller.get(request, response);
-
-      expect(response.render).toHaveBeenCalledWith(
-        TranslationKeys.CHECK_YOUR_ANSWERS_EMPLOYERS_CONTRACT_CLAIM,
-        expect.anything()
-      );
       expect(response.render).toHaveBeenCalledWith(
         TranslationKeys.CHECK_YOUR_ANSWERS_EMPLOYERS_CONTRACT_CLAIM,
         expect.anything()
@@ -49,32 +46,31 @@ describe('CheckYourAnswersEmployersContractClaimController', () => {
 
   describe('POST method', () => {
     it('should go to the respondent response task list on valid submission', async () => {
-      request.body = {
-        employersContractClaimSection: 'Yes', // Ensure this is set
-      };
-
-      (ET3Util.updateET3Data as jest.Mock).mockResolvedValue(userCase); // Mock the ET3 update function
-
+      updateET3ResponseWithET3FormMock.mockImplementation(
+        createMockedUpdateET3ResponseWithET3FormFunction(
+          PageUrls.RESPONDENT_RESPONSE_TASK_LIST,
+          request,
+          response,
+          [],
+          mockCaseWithIdWithRespondents
+        )
+      );
       await controller.post(request, response);
 
-      expect(request.session.userCase).toEqual(userCase); // Validate the userCase is set
+      expect(request.session.userCase).toEqual(mockCaseWithIdWithRespondents); // Validate the userCase is set
       expect(response.redirect).toHaveBeenCalledWith(PageUrls.RESPONDENT_RESPONSE_TASK_LIST); // Ensure the correct redirect occurs
     });
 
     it('should redirect back to Employers Contract Claim if ET3 data update fails', async () => {
-      // Simulate validation errors
-      const mockFormError: FormError = {
-        propertyName: 'employersContractClaimSection',
-        errorType: 'required',
-      };
-
-      request.body = {
-        employersContractClaimSection: '', // Simulate invalid input
-      };
-
-      // Simulate an error during ET3 update
-      (ET3Util.updateET3Data as jest.Mock).mockRejectedValue(mockFormError);
-
+      updateET3ResponseWithET3FormMock.mockImplementation(
+        createMockedUpdateET3ResponseWithET3FormFunction(
+          request.url,
+          request,
+          response,
+          [mockFormError],
+          mockCaseWithIdWithRespondents
+        )
+      );
       await controller.post(request, response);
 
       expect(response.redirect).toHaveBeenCalledWith(request.url);
