e8c26cb454079fa8f3038ae5f19572750dd83d2a
mehmet.dede <mehmet.dede@justice.gov.uk>
2024-10-22 17:27:05 +0100

    RET-4329 updates

--- src/main/controllers/ET1FormViewController.ts
+++ src/main/controllers/ET1FormViewController.ts
@@ -8,7 +8,11 @@ import ET3Util from '../utils/ET3Util';
 
 export default class ET1FormViewController {
   public async get(req: AppRequest, res: Response): Promise<void> {
-    await ET3Util.updateCaseDetailsLinkStatuses(req, ET3CaseDetailsLinkNames.ET1ClaimForm, LinkStatus.VIEWED);
+    req.session.userCase = await ET3Util.updateCaseDetailsLinkStatuses(
+      req,
+      ET3CaseDetailsLinkNames.ET1ClaimForm,
+      LinkStatus.VIEWED
+    );
     const languageParam = getLanguageParam(req.url);
     if (languageParam === languages.WELSH_URL_PARAMETER) {
       res.redirect(req.session.et1FormWelsh.value.uploadedDocument.document_binary_url);
