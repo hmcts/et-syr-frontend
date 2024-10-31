ca4984c1b4e43180f49f7feb0ee6125cb0ef8ebd
Mike-Brain <Michael.brain1@justice.gov.uk>
2024-10-31 18:02:28 +0000

    Fortify Issues
    * fix merge issues

--- src/main/utils/ET3Util.ts
+++ src/main/utils/ET3Util.ts
@@ -14,6 +14,7 @@ import {
 import { ET3CaseDetailsLinkNames, LinkStatus } from '../definitions/links';
 import { formatApiCaseDataToCaseWithId } from '../helpers/ApiFormatter';
 import { setUserCase } from '../helpers/CaseHelpers';
+import { setUrlLanguage } from '../helpers/LanguageHelper';
 import { returnNextPage, returnValidUrl } from '../helpers/RouterHelpers';
 import { getLogger } from '../logger';
 import { getCaseApi } from '../services/CaseService';
