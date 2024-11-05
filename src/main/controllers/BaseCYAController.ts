b3674f9f929440c3aace93fcf4968b4208b24329
Mike-Brain <Michael.brain1@justice.gov.uk>
2024-11-05 16:57:48 +0000

    Check your answers & accessibility
    * refactor CYA to avoid duplications

---
+++ src/main/controllers/BaseCYAController.ts
@@ -0,0 +1,43 @@
+import { Form } from '../components/form';
+import { YesOrNo } from '../definitions/case';
+import { FormContent, FormFields } from '../definitions/form';
+import { saveForLaterButton, submitButton } from '../definitions/radios';
+import { AnyRecord } from '../definitions/util-types';
+import { isOptionSelected } from '../validators/validator';
+
+export default abstract class BaseCYAController {
+  protected readonly form: Form;
+  protected readonly formContent: FormContent;
+
+  constructor(sectionName: string) {
+    this.formContent = {
+      fields: {
+        [sectionName]: {
+          classes: 'govuk-radios',
+          id: sectionName,
+          type: 'radios',
+          label: (l: AnyRecord): string => l.cya.label,
+          hint: (l: AnyRecord): string => l.cya.hint,
+          labelHidden: false,
+          values: [
+            {
+              name: sectionName,
+              label: (l: AnyRecord): string => l.cya.yes,
+              value: YesOrNo.YES,
+            },
+            {
+              name: sectionName,
+              label: (l: AnyRecord): string => l.cya.no,
+              value: YesOrNo.NO,
+            },
+          ],
+          validator: isOptionSelected,
+        },
+      },
+      submit: submitButton,
+      saveForLater: saveForLaterButton,
+    } as never;
+
+    this.form = new Form(<FormFields>this.formContent.fields);
+  }
+}
--- src/main/controllers/CheckYourAnswersContestClaimController.ts
+++ src/main/controllers/CheckYourAnswersContestClaimController.ts
@@ -1,51 +1,20 @@
 import { Response } from 'express';
 
-import { Form } from '../components/form';
 import { AppRequest } from '../definitions/appRequest';
 import { YesOrNo } from '../definitions/case';
 import { InterceptPaths, PageUrls, TranslationKeys } from '../definitions/constants';
-import { FormContent, FormFields } from '../definitions/form';
 import { ET3HubLinkNames, LinkStatus } from '../definitions/links';
-import { saveForLaterButton, submitButton } from '../definitions/radios';
 import { AnyRecord } from '../definitions/util-types';
 import { setUrlLanguage } from '../helpers/LanguageHelper';
 import { conditionalRedirect } from '../helpers/RouterHelpers';
 import { getEt3Section5 } from '../helpers/controller/CheckYourAnswersET3Helper';
 import ET3Util from '../utils/ET3Util';
-import { isOptionSelected } from '../validators/validator';
 
-export default class CheckYourAnswersContestClaimController {
-  private readonly form: Form;
-  private readonly formContent: FormContent = {
-    fields: {
-      contestClaimSection: {
-        classes: 'govuk-radios',
-        id: 'contestClaimSection',
-        type: 'radios',
-        label: (l: AnyRecord): string => l.cya.label,
-        hint: (l: AnyRecord): string => l.cya.hint,
-        labelHidden: false,
-        values: [
-          {
-            name: 'contestClaimSection',
-            label: (l: AnyRecord): string => l.cya.yes,
-            value: YesOrNo.YES,
-          },
-          {
-            name: 'contestClaimSection',
-            label: (l: AnyRecord): string => l.cya.no,
-            value: YesOrNo.NO,
-          },
-        ],
-        validator: isOptionSelected,
-      },
-    },
-    submit: submitButton,
-    saveForLater: saveForLaterButton,
-  } as never;
+import BaseCYAController from './BaseCYAController';
 
+export default class CheckYourAnswersContestClaimController extends BaseCYAController {
   constructor() {
-    this.form = new Form(<FormFields>this.formContent.fields);
+    super('contestClaimSection');
   }
 
   public post = async (req: AppRequest, res: Response): Promise<void> => {
