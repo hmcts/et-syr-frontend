4295b511bf0ea72394c7c203a4502345ac223b80
mehmet.dede <mehmet.dede@justice.gov.uk>
2024-11-13 14:36:51 +0000

    Updates to not send employment start and end dates when date fields are empty or not entered correctly.

--- src/test/unit/mocks/mockRespondentType.ts
+++ src/test/unit/mocks/mockRespondentType.ts
@@ -1,5 +1,11 @@
+import { RespondentType } from '../../../main/definitions/complexTypes/respondent';
+
 import mockRespondentType from './mockRespondentType.json';
+import mockRespondentTypeWithoutCheckedFields from './mockRespondentTypeWithoutCheckedFields.json';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 export const mockedRespondentType: RespondentType = mockRespondentType;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+export const mockedRespondentTypeWithoutCheckedFields: RespondentType = mockRespondentTypeWithoutCheckedFields;
--- src/test/unit/utils/ET3DataModelUtil.test.ts
+++ src/test/unit/utils/ET3DataModelUtil.test.ts
@@ -5,7 +5,7 @@ import {
   mockCaseWithIdForET3DataModelUtilConvertSelectedRespondentToRespondentTypeTest,
   mockCaseWithIdWithRespondents,
 } from '../mocks/mockCaseWithId';
-import { mockedRespondentType } from '../mocks/mockRespondentType';
+import { mockedRespondentType, mockedRespondentTypeWithoutCheckedFields } from '../mocks/mockRespondentType';
 
 describe('ET3DataModelUtil tests', () => {
   const caseWithId: CaseWithId = mockCaseWithIdForET3DataModelUtilConvertSelectedRespondentToRespondentTypeTest;
@@ -16,5 +16,30 @@ describe('ET3DataModelUtil tests', () => {
       selectedRespondent
     );
     expect(respondentType).toEqual(mockedRespondentType);
+  });
+  test('convertSelectedRespondentToRespondentType without checked fields', () => {
+    caseWithId.et3ResponseEmploymentStartDate = undefined;
+    caseWithId.et3ResponseEmploymentEndDate = undefined;
+    caseWithId.idamId = undefined;
+    caseWithId.et3Status = undefined;
+    caseWithId.et3CompanyHouseDocumentUrl = undefined;
+    caseWithId.et3CompanyHouseDocumentBinaryUrl = undefined;
+    caseWithId.et3IndividualInsolvencyDocumentBinaryUrl = undefined;
+    caseWithId.et3IndividualInsolvencyDocumentUrl = undefined;
+    caseWithId.et3VettingDocumentBinaryUrl = undefined;
+    caseWithId.et3VettingDocumentUrl = undefined;
+    caseWithId.et3ResponseEmployerClaimDocumentBinaryUrl = undefined;
+    caseWithId.et3ResponseEmployerClaimDocumentUrl = undefined;
+    caseWithId.et3ResponseRespondentSupportDocumentBinaryUrl = undefined;
+    caseWithId.et3ResponseRespondentSupportDocument = undefined;
+    caseWithId.et3FormBinaryUrl = undefined;
+    caseWithId.et3FormUrl = undefined;
+    caseWithId.et3FormWelshBinaryUrl = undefined;
+    caseWithId.et3FormWelshUrl = undefined;
+    const respondentType: RespondentType = ET3DataModelUtil.convertSelectedRespondentToRespondentType(
+      caseWithId,
+      selectedRespondent
+    );
+    expect(respondentType).toEqual(mockedRespondentTypeWithoutCheckedFields);
   });
 });
