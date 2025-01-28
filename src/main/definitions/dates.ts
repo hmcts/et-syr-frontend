495deace29b07461a0adf7da3479a7d636718396
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2025-01-28 15:56:49 +0000

    Revert

--- src/main/definitions/dates.ts
+++ src/main/definitions/dates.ts
@@ -1,6 +1,19 @@
-import { DateValidator } from '../validators/dateValidators';
+import {
+  DateValidator,
+  areDateFieldsFilledIn,
+  areDates10YearsApartOrMore,
+  convertDateToCaseDate,
+  isDateEmpty,
+  isDateInLastTenYears,
+  isDateInNextTenYears,
+  isDateInPast,
+  isDateInputInvalid,
+  isDateNotInPast,
+  isDateNotPartial,
+} from '../validators/dateValidators';
 
 import { CaseDate } from './case';
+import { InvalidField } from './form';
 import { AnyRecord, UnknownRecord } from './util-types';
 
 export const DateValues = [
@@ -35,4 +48,67 @@ export type DateFormFields = {
   values: typeof DateValues;
   parser: (body: UnknownRecord) => CaseDate;
   validator: DateValidator;
+};
+
+type DateTypes = string | void | InvalidField;
+
+export const BirthDateFormFields = {
+  classes: 'govuk-date-input',
+  type: 'date',
+  label: (l: AnyRecord): string => l.legend,
+  labelHidden: false,
+  labelSize: 'l',
+  hint: (l: AnyRecord): string => l.hint,
+  values: DateValues,
+  validator: (value: CaseDate): DateTypes =>
+    isDateNotPartial(value) ||
+    (isDateEmpty(value) ? '' : isDateInputInvalid(value)) ||
+    isDateInPast(value) ||
+    areDates10YearsApartOrMore(value, convertDateToCaseDate(new Date())),
+};
+
+export const EndDateFormFields = {
+  classes: 'govuk-date-input',
+  type: 'date',
+  label: (l: AnyRecord): string => l.legend,
+  labelHidden: false,
+  labelSize: 'l',
+  hint: (l: AnyRecord): string => l.hint,
+  values: DateValues,
+  validator: (value: CaseDate): DateTypes =>
+    areDateFieldsFilledIn(value) || isDateInputInvalid(value) || isDateInPast(value) || isDateInLastTenYears(value),
+};
+
+export const NewJobDateFormFields = {
+  classes: 'govuk-date-input',
+  type: 'date',
+  label: (l: AnyRecord): string => l.legend,
+  labelHidden: false,
+  labelSize: 'l',
+  values: DateValues,
+  validator: (value: CaseDate): DateTypes => isDateInputInvalid(value) || isDateInNextTenYears(value),
+};
+
+export const NoticeEndDateFormFields = {
+  classes: 'govuk-date-input',
+  type: 'date',
+  label: (l: AnyRecord): string => l.legend,
+  labelHidden: false,
+  labelSize: 'l',
+  hint: (l: AnyRecord): string => l.hint,
+  values: DateValues,
+  validator: (value: CaseDate): DateTypes =>
+    areDateFieldsFilledIn(value) || isDateInputInvalid(value) || isDateNotInPast(value) || isDateInNextTenYears(value),
+};
+
+export const StartDateFormFields = {
+  classes: 'govuk-date-input',
+  type: 'date',
+  label: (l: AnyRecord): string => l.legend,
+  labelHidden: false,
+  labelSize: 'l',
+  hint: (l: AnyRecord): string => l.hint,
+  values: DateValues,
+  validator: (value: CaseDate): DateTypes =>
+    areDateFieldsFilledIn(value) || isDateInputInvalid(value) || isDateInPast(value),
 };
--- src/main/definitions/radios.ts
+++ src/main/definitions/radios.ts
@@ -1,6 +1,6 @@
-import { isContentCharsOrLessAndNotEmpty, isOptionSelected } from '../validators/validator';
+import { Validator, isContentCharsOrLessAndNotEmpty, isFieldFilledIn, isOptionSelected } from '../validators/validator';
 
-import { YesOrNo } from './case';
+import { PayInterval, YesOrNo } from './case';
 import { SubmitButton } from './form';
 import { AnyRecord } from './util-types';
 
@@ -29,6 +29,47 @@ export const CopyToOtherPartyRadioFormFields = {
   validator: isOptionSelected,
 };
 
+export const PayIntervalRadioValues = [
+  {
+    label: (l: AnyRecord): string => l.weekly,
+    value: PayInterval.WEEKS,
+  },
+  {
+    label: (l: AnyRecord): string => l.monthly,
+    value: PayInterval.MONTHS,
+  },
+  {
+    label: (l: AnyRecord): string => l.annual,
+    value: PayInterval.ANNUAL,
+  },
+];
+
+export const YesNoRadioValues = [
+  {
+    label: (l: AnyRecord): string => l.yes,
+    name: 'radioYes',
+    value: YesOrNo.YES,
+  },
+  {
+    label: (l: AnyRecord): string => l.no,
+    name: 'radioNo',
+    value: YesOrNo.NO,
+  },
+];
+
+export const SupportingMaterialYesNoRadioValues = [
+  {
+    label: (l: AnyRecord): string => l.supportingMaterialYesNo.yes,
+    name: 'radioYes',
+    value: YesOrNo.YES,
+  },
+  {
+    label: (l: AnyRecord): string => l.supportingMaterialYesNo.no,
+    name: 'radioNo',
+    value: YesOrNo.NO,
+  },
+];
+
 export const saveAndContinueButton: SubmitButton = {
   text: (l: AnyRecord): string => l.saveAndContinue,
   classes: 'govuk-!-margin-right-2',
@@ -37,4 +78,41 @@ export const saveAndContinueButton: SubmitButton = {
 export const saveForLaterButton: SubmitButton = {
   text: (l: AnyRecord): string => l.saveForLater,
   classes: 'govuk-button--secondary',
+};
+
+export type RadioFormFields = {
+  id: string;
+  classes: string;
+  type: string;
+  label: (l: AnyRecord) => string;
+  labelHidden?: boolean;
+  values: typeof YesNoRadioValues;
+  validator: Validator;
+};
+
+export const DefaultRadioFormFields = {
+  type: 'radios',
+  values: YesNoRadioValues,
+  validator: isFieldFilledIn,
+};
+
+export const DefaultInlineRadioFormFields = {
+  classes: 'govuk-radios--inline',
+  type: 'radios',
+  values: YesNoRadioValues,
+  validator: isFieldFilledIn,
+};
+
+export type PayIntervalRadioFormFields = {
+  id: string;
+  classes: string;
+  type: string;
+  label: (l: AnyRecord) => string;
+  values: typeof PayIntervalRadioValues;
+};
+
+export const DefaultPayIntervalRadioFormFields = {
+  type: 'radios',
+  classes: 'govuk-radios',
+  values: PayIntervalRadioValues,
 };
