71ea94f610570729dfe6e21d25444eb14d79385c
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2025-03-17 11:24:58 +0000

    RET-5698 (#622)

--- src/main/helpers/ApplicationStateHelper.ts
+++ src/main/helpers/ApplicationStateHelper.ts
@@ -1,10 +1,11 @@
 import { UserDetails } from '../definitions/appRequest';
 import { YesOrNo } from '../definitions/case';
 import { GenericTseApplicationType } from '../definitions/complexTypes/genericTseApplicationTypeItem';
+import { Applicant } from '../definitions/constants';
 import { LinkStatus } from '../definitions/links';
-import CollectionUtils from '../utils/CollectionUtils';
 
 import { isResponseToTribunalRequired } from './GenericTseApplicationHelper';
+import { isDecisionShareToRespondent } from './controller/ClaimantsApplicationsHelper';
 import { isYourApplication } from './controller/YourRequestAndApplicationsHelper';
 
 /**
@@ -13,16 +14,11 @@ import { isYourApplication } from './controller/YourRequestAndApplicationsHelper
  * @param user current user
  */
 export const getApplicationState = (app: GenericTseApplicationType, user: UserDetails): LinkStatus => {
-  if (CollectionUtils.isNotEmpty(app.respondentState)) {
-    const existingState = app.respondentState.find(state => state.value.userIdamId === user.id);
-    if (existingState) {
-      return <LinkStatus>existingState.value.applicationState;
-    }
+  const existingState = app?.respondentState?.find(state => state.value.userIdamId === user.id);
+  if (existingState?.value?.applicationState) {
+    return existingState.value.applicationState as LinkStatus;
   }
-  if (app?.applicantIdamId === user?.id) {
-    return LinkStatus.IN_PROGRESS;
-  }
-  return LinkStatus.NOT_STARTED_YET;
+  return app?.applicantIdamId === user?.id ? LinkStatus.IN_PROGRESS : LinkStatus.NOT_STARTED_YET;
 };
 
 /**
@@ -31,17 +27,59 @@ export const getApplicationState = (app: GenericTseApplicationType, user: UserDe
  * @param user current user
  */
 export const getApplicationStatusAfterViewed = (app: GenericTseApplicationType, user: UserDetails): LinkStatus => {
-  const currentState = getApplicationState(app, user);
+  if (!app || !user) {
+    return undefined;
+  }
+
+  const existingState = app.respondentState?.find(state => state.value.userIdamId === user.id);
+  const currentState: LinkStatus = existingState?.value?.applicationState as LinkStatus;
+
   if (currentState === LinkStatus.NOT_VIEWED) {
     return LinkStatus.VIEWED;
-  } else if (currentState === LinkStatus.UPDATED) {
+  }
+
+  if (currentState === LinkStatus.UPDATED) {
     return app.claimantResponseRequired === YesOrNo.YES ? LinkStatus.IN_PROGRESS : LinkStatus.WAITING_FOR_TRIBUNAL;
-  } else if (
+  }
+
+  if (
     currentState === LinkStatus.NOT_STARTED_YET &&
     isYourApplication(app, user) &&
     isResponseToTribunalRequired(app, user)
   ) {
     return LinkStatus.IN_PROGRESS;
   }
+
   return undefined;
+};
+
+/**
+ * Get application state if not exist in application
+ * @param app application
+ * @param user current user
+ */
+export const getApplicationStateIfNotExist = (app: GenericTseApplicationType, user: UserDetails): LinkStatus => {
+  if (isResponseToTribunalRequired(app, user)) {
+    return LinkStatus.NOT_STARTED_YET;
+  }
+
+  const adminDecisionExist = app.adminDecision?.some(d => isDecisionShareToRespondent(d.value));
+  if (adminDecisionExist) {
+    return LinkStatus.NOT_VIEWED;
+  }
+
+  const isYours = isYourApplication(app, user);
+  if (!isYours) {
+    return LinkStatus.NOT_STARTED_YET;
+  }
+
+  if (app.respondCollection?.length === 0) {
+    return LinkStatus.IN_PROGRESS;
+  }
+
+  if (app.respondCollection?.some(r => r.value.from === Applicant.ADMIN || r.value.fromIdamId !== user.id)) {
+    return LinkStatus.UPDATED;
+  }
+
+  return LinkStatus.IN_PROGRESS;
 };
--- src/main/helpers/controller/CaseDetailsHelper.ts
+++ src/main/helpers/controller/CaseDetailsHelper.ts
@@ -1,48 +1,96 @@
-import { AppRequest } from '../../definitions/appRequest';
+import { AppRequest, UserDetails } from '../../definitions/appRequest';
+import { GenericTseApplicationTypeItem } from '../../definitions/complexTypes/genericTseApplicationTypeItem';
 import { ET3CaseDetailsLinkNames, ET3CaseDetailsLinksStatuses, LinkStatus } from '../../definitions/links';
-import { getLogger } from '../../logger';
-import ET3Util from '../../utils/ET3Util';
+import { getCaseApi } from '../../services/CaseService';
+import { getApplicationStateIfNotExist } from '../ApplicationStateHelper';
 
 import { isClaimantApplicationShare } from './ClaimantsApplicationsHelper';
 import { isOtherRespApplicationShare } from './OtherRespondentsApplicationsHelper';
+import { isYourApplication } from './YourRequestAndApplicationsHelper';
 
-const logger = getLogger('CaseDetailsController');
+const priorityOrder = [
+  LinkStatus.NOT_STARTED_YET,
+  LinkStatus.NOT_VIEWED,
+  LinkStatus.UPDATED,
+  LinkStatus.IN_PROGRESS,
+  LinkStatus.VIEWED,
+  LinkStatus.WAITING_FOR_TRIBUNAL,
+];
 
 export const getET3CaseDetailsLinkNames = async (
   statuses: ET3CaseDetailsLinksStatuses,
   req: AppRequest
 ): Promise<ET3CaseDetailsLinksStatuses> => {
   const apps = req.session?.userCase?.genericTseApplicationCollection;
-
-  if (!(ET3CaseDetailsLinkNames.OtherRespondentApplications in statuses)) {
-    statuses[ET3CaseDetailsLinkNames.OtherRespondentApplications] = LinkStatus.NOT_YET_AVAILABLE;
-  }
-
-  if (
-    statuses[ET3CaseDetailsLinkNames.ClaimantApplications] === LinkStatus.NOT_YET_AVAILABLE &&
-    apps?.some(app => isClaimantApplicationShare(app.value))
-  ) {
-    logger.info('Update ET3CaseDetailsLinkNames.ClaimantApplications');
-    statuses[ET3CaseDetailsLinkNames.ClaimantApplications] = LinkStatus.IN_PROGRESS;
-    await ET3Util.updateCaseDetailsLinkStatuses(
-      req,
-      ET3CaseDetailsLinkNames.ClaimantApplications,
-      LinkStatus.IN_PROGRESS
-    );
-  }
-
-  if (
-    statuses[ET3CaseDetailsLinkNames.OtherRespondentApplications] === LinkStatus.NOT_YET_AVAILABLE &&
-    apps?.some(app => isOtherRespApplicationShare(app.value, req.session.user))
-  ) {
-    logger.info('Update ET3CaseDetailsLinkNames.OtherRespondentApplications');
-    statuses[ET3CaseDetailsLinkNames.OtherRespondentApplications] = LinkStatus.IN_PROGRESS;
-    await ET3Util.updateCaseDetailsLinkStatuses(
-      req,
-      ET3CaseDetailsLinkNames.OtherRespondentApplications,
-      LinkStatus.IN_PROGRESS
-    );
-  }
-
+  await updateApplicationsStatusIfNotExist(apps, req);
+  updateYourRequestsAndApplications(statuses, apps, req.session.user);
+  updateClaimantAppsLinkStatus(statuses, apps, req.session.user);
+  updateOtherRespondentAppsLinkStatus(statuses, apps, req.session.user);
   return statuses;
+};
+
+const updateApplicationsStatusIfNotExist = async (
+  allApps: GenericTseApplicationTypeItem[],
+  req: AppRequest
+): Promise<void> => {
+  const user = req.session?.user;
+  const filteredApps =
+    allApps?.filter(app => !app.value?.respondentState?.some(state => state.value?.userIdamId === user?.id)) || [];
+  for (const app of filteredApps) {
+    const newState: LinkStatus = getApplicationStateIfNotExist(app.value, req.session.user);
+    await getCaseApi(req.session.user?.accessToken).changeApplicationStatus(req, app, newState);
+  }
+};
+
+const updateYourRequestsAndApplications = (
+  statuses: ET3CaseDetailsLinksStatuses,
+  allApps: GenericTseApplicationTypeItem[],
+  user: UserDetails
+): void => {
+  const apps = allApps?.filter(app => isYourApplication(app.value, user)) || [];
+  statuses[ET3CaseDetailsLinkNames.YourRequestsAndApplications] = getLinkStatus(apps, user, true);
+};
+
+const updateClaimantAppsLinkStatus = (
+  statuses: ET3CaseDetailsLinksStatuses,
+  allApps: GenericTseApplicationTypeItem[],
+  user: UserDetails
+): void => {
+  const apps = allApps?.filter(app => isClaimantApplicationShare(app.value)) || [];
+  statuses[ET3CaseDetailsLinkNames.ClaimantApplications] = getLinkStatus(apps, user, false);
+};
+
+const updateOtherRespondentAppsLinkStatus = (
+  statuses: ET3CaseDetailsLinksStatuses,
+  allApps: GenericTseApplicationTypeItem[],
+  user: UserDetails
+): void => {
+  const apps = allApps?.filter(app => isOtherRespApplicationShare(app.value, user)) || [];
+  statuses[ET3CaseDetailsLinkNames.OtherRespondentApplications] = getLinkStatus(apps, user, false);
+};
+
+const getLinkStatus = (apps: GenericTseApplicationTypeItem[], user: UserDetails, isYours: boolean): LinkStatus => {
+  if (!apps?.length) {
+    return LinkStatus.NOT_YET_AVAILABLE;
+  }
+
+  const userApplicationStates = getUserApplicationStates(apps, user);
+  for (const status of priorityOrder) {
+    if (userApplicationStates.includes(status)) {
+      return status;
+    }
+  }
+
+  return isYours ? LinkStatus.IN_PROGRESS : LinkStatus.NOT_STARTED_YET;
+};
+
+const getUserApplicationStates = (apps: GenericTseApplicationTypeItem[], user: UserDetails): string[] => {
+  return (
+    apps?.flatMap(
+      app =>
+        app.value?.respondentState
+          ?.filter(state => state.value?.userIdamId === user?.id)
+          .map(state => state.value?.applicationState) || []
+    ) || []
+  );
 };
