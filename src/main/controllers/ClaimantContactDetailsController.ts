87f9500679fc7c95fcae9e0d51aa780ef15b8dd2
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2025-04-14 15:19:31 +0100

    Update ClaimantContactDetailsController.ts

--- src/main/controllers/ClaimantContactDetailsController.ts
+++ src/main/controllers/ClaimantContactDetailsController.ts
@@ -8,6 +8,7 @@ export default class ClaimantContactDetailsController {
   public get = async (req: AppRequest, res: Response): Promise<void> => {
     res.render(TranslationKeys.CLAIMANT_CONTACT_DETAILS, {
       ...req.t(TranslationKeys.COMMON as never, { returnObjects: true } as never),
+      ...req.t(TranslationKeys.SIDEBAR_CONTACT_US as never, { returnObjects: true } as never),
       ...req.t(TranslationKeys.CLAIMANT_CONTACT_DETAILS as never, { returnObjects: true } as never),
       hideContactUs: true,
       claimantContactDetails: getClaimantContactDetails(req),
