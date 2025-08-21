a1121af5fa685d861abba9bb22deccb93f490c55
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2025-08-21 15:10:01 +0100

    Update CopyToOtherPartyOfflineController.ts

--- src/main/controllers/CopyToOtherPartyOfflineController.ts
+++ src/main/controllers/CopyToOtherPartyOfflineController.ts
@@ -3,60 +3,20 @@ import { Response } from 'express';
 import { Form } from '../components/form';
 import { AppRequest } from '../definitions/appRequest';
 import { continueButton } from '../definitions/buttons';
-import { CaseWithId, YesOrNo } from '../definitions/case';
+import { CaseWithId } from '../definitions/case';
 import { PageUrls, TranslationKeys } from '../definitions/constants';
 import { FormContent, FormFields } from '../definitions/form';
-import { AnyRecord } from '../definitions/util-types';
+import { CopyToOtherPartyOfflineRadioFormFields } from '../definitions/radios';
 import { getApplicationDisplayByCode } from '../helpers/ApplicationHelper';
 import { getPageContent } from '../helpers/FormHelper';
 import { getLanguageParam } from '../helpers/RouterHelpers';
 import UrlUtils from '../utils/UrlUtils';
-import { isContentCharsOrLessAndNotEmpty, isOptionSelected } from '../validators/validator';
 
 export default class CopyToOtherPartyOfflineController {
   private readonly form: Form;
   private readonly formContent: FormContent = {
     fields: {
-      copyToOtherPartyYesOrNo: {
-        type: 'radios',
-        label: (l: AnyRecord): string => l.copyToOtherPartyYesOrNo.label,
-        values: [
-          {
-            label: (l: AnyRecord): string =>
-              '<p class="govuk-body">' +
-              l.copyToOtherPartyYesOrNo.yes1 +
-              '</p><p class="govuk-body"><strong>' +
-              l.copyToOtherPartyYesOrNo.important +
-              ':</strong> ' +
-              l.copyToOtherPartyYesOrNo.yes2 +
-              '</p><p class="govuk-body">' +
-              l.copyToOtherPartyYesOrNo.yes3 +
-              '</p>',
-            value: YesOrNo.YES,
-          },
-          {
-            label: (l: AnyRecord): string =>
-              '<p class="govuk-body">' +
-              l.noIDoNotWantTo +
-              '</p><p class="govuk-body"><strong>' +
-              l.important +
-              ':</strong> ' +
-              l.youMustTellTheTribunal +
-              '</p>',
-            value: YesOrNo.NO,
-            subFields: {
-              copyToOtherPartyText: {
-                type: 'charactercount',
-                label: (l: AnyRecord): string => l.copyToOtherPartyText.label,
-                labelSize: 's',
-                maxlength: 2500,
-                validator: isContentCharsOrLessAndNotEmpty(2500),
-              },
-            },
-          },
-        ],
-        validator: isOptionSelected,
-      },
+      copyToOtherPartyYesOrNo: CopyToOtherPartyOfflineRadioFormFields,
     },
     submit: continueButton,
   };
--- src/main/definitions/radios.ts
+++ src/main/definitions/radios.ts
@@ -4,6 +4,14 @@ import { YesOrNo } from './case';
 import { SubmitButton } from './form';
 import { AnyRecord } from './util-types';
 
+const CopyToOtherPartyTextFormFields = {
+  type: 'charactercount',
+  label: (l: AnyRecord): string => l.copyToOtherPartyText.label,
+  labelSize: 's',
+  maxlength: 2500,
+  validator: isContentCharsOrLessAndNotEmpty(2500),
+};
+
 export const CopyToOtherPartyRadioFormFields = {
   type: 'radios',
   label: (l: AnyRecord): string => l.copyToOtherPartyYesOrNo.label,
@@ -16,13 +24,42 @@ export const CopyToOtherPartyRadioFormFields = {
       label: (l: AnyRecord): string => l.copyToOtherPartyYesOrNo.no,
       value: YesOrNo.NO,
       subFields: {
-        copyToOtherPartyText: {
-          type: 'charactercount',
-          label: (l: AnyRecord): string => l.copyToOtherPartyText.label,
-          labelSize: 's',
-          maxlength: 2500,
-          validator: isContentCharsOrLessAndNotEmpty(2500),
-        },
+        copyToOtherPartyText: CopyToOtherPartyTextFormFields,
+      },
+    },
+  ],
+  validator: isOptionSelected,
+};
+
+export const CopyToOtherPartyOfflineRadioFormFields = {
+  type: 'radios',
+  label: (l: AnyRecord): string => l.copyToOtherPartyYesOrNo.label,
+  values: [
+    {
+      label: (l: AnyRecord): string =>
+        '<p class="govuk-body">' +
+        l.copyToOtherPartyYesOrNo.yes1 +
+        '</p><p class="govuk-body"><strong>' +
+        l.copyToOtherPartyYesOrNo.important +
+        ':</strong> ' +
+        l.copyToOtherPartyYesOrNo.yes2 +
+        '</p><p class="govuk-body">' +
+        l.copyToOtherPartyYesOrNo.yes3 +
+        '</p>',
+      value: YesOrNo.YES,
+    },
+    {
+      label: (l: AnyRecord): string =>
+        '<p class="govuk-body">' +
+        l.noIDoNotWantTo +
+        '</p><p class="govuk-body"><strong>' +
+        l.important +
+        ':</strong> ' +
+        l.youMustTellTheTribunal +
+        '</p>',
+      value: YesOrNo.NO,
+      subFields: {
+        copyToOtherPartyText: CopyToOtherPartyTextFormFields,
       },
     },
   ],
