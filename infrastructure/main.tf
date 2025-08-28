8078693093f4ba32e3a49b9c41dc4e519077fd00
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2025-08-28 09:08:51 +0100

    Refactor Redis configuration and remove unused variables

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,7 +9,7 @@ locals {
     tomap({
       "environment"  = local.tagEnv,
       "managedBy"    = "Employment Tribunals",
-      "Team Contact" = "#et-devs",
+      "Team Contact" = "#et-ci-devs",
       "application"  = "employment-tribunals",
       "businessArea" = "CFT",
       "builtFrom"    = "et-syr"
@@ -17,29 +17,7 @@ locals {
   )
 }
 
-module "et-frontend-session-storage" {
-  source                        = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product                       = "${var.product}-${var.component}-session-storage"
-  location                      = var.location
-  env                           = var.env
-  private_endpoint_enabled      = true
-  redis_version                 = "6"
-  business_area                 = "cft"
-  public_network_access_enabled = false
-  common_tags                   = var.common_tags
-  sku_name                      = var.sku_name
-  family                        = var.family
-  capacity                      = var.capacity
-
-}
-
 data "azurerm_key_vault" "key_vault" {
   name                = local.vaultName
   resource_group_name = local.vaultName
-}
-
-resource "azurerm_key_vault_secret" "redis_access_key" {
-  name         = "et-syr-redis-access-key"
-  value        = module.et-frontend-session-storage.access_key
-  key_vault_id = data.azurerm_key_vault.key_vault.id
 }
