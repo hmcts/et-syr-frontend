a5f0837c355d6fd7f69eb9bf9df548b9e4e7517e
Mehmet Tahir Dede <mehmet.dede@justice.gov.uk>
2025-11-04 13:37:31 +0000

    Updates to merge with master branch.

--- src/main/controllers/ContactTribunalController.ts
+++ src/main/controllers/ContactTribunalController.ts
@@ -6,29 +6,19 @@ import { getApplicationsAccordionItems } from '../helpers/controller/ContactTrib
 
 export default class ContactTribunalController {
   public get = async (req: AppRequest, res: Response): Promise<void> => {
-    const isContactTribunalEnabled = await getFlagValue('et3-contact-tribunal', null);
     const respondentRepresented = req.session.userCase?.respondentRepresented;
 
-    if (!isContactTribunalEnabled) {
-      return res.redirect(PageUrls.HOLDING_PAGE + getLanguageParam(req.url));
-    }
-
-    if (!isClaimantSystemUser(req.session.userCase)) {
-      return res.redirect(PageUrls.HOLDING_PAGE + getLanguageParam(req.url));
-    }
-
-    const applicationsAccordionItems =
-      respondentRepresented === undefined
-        ? getApplicationsAccordionItems(req.url, {
-            ...req.t(TranslationKeys.CONTACT_TRIBUNAL, { returnObjects: true }),
-          })
-        : [];
     res.render(TranslationKeys.CONTACT_TRIBUNAL, {
       ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
       ...req.t(TranslationKeys.CONTACT_TRIBUNAL, { returnObjects: true }),
       ...req.t(TranslationKeys.SIDEBAR_CONTACT_US, { returnObjects: true }),
       hideContactUs: true,
-      applicationsAccordionItems,
+      applicationsAccordionItems:
+        respondentRepresented === undefined
+          ? getApplicationsAccordionItems(req.url, {
+              ...req.t(TranslationKeys.CONTACT_TRIBUNAL, { returnObjects: true }),
+            })
+          : [],
       respondentRepresented,
     });
   };
