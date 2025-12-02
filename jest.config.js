9636b0204fd46718409b0feca60d6523516e7970
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2025-12-02 14:34:45 +0000

    Use v8 coverage provider to fix babel-plugin-istanbul incompatibility with SWC

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   projects: ['<rootDir>/jest.unit.config.js', '<rootDir>/jest.views.config.js'],
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['src/main/**/*.{js,ts}', '!src/main/**/*.d.ts', '!src/main/assets/**'],
 };
