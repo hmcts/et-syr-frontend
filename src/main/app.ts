690bdafa94f99b1a19eb1f64e618bc6befbaca74
sonny-hmcts <sonny.lloyd@hmcts.net>
2026-06-29 17:19:48 +0100

    allow redirect to cui urls in helmet

--- src/main/app.ts
+++ src/main/app.ts
@@ -39,6 +39,7 @@ new Helmet(config.get('security'), [
   process.env.PCQ_URL ?? config.get('services.pcq.url'),
   process.env.ET1_BASE_URL ?? config.get('services.et1Legacy.url'),
   process.env.ET3_LEGACY_URL ? config.get('services.et1Legacy.url') : LegacyUrls.SIGN_UP,
+  process.env.CUI_URL ?? config.get('services.cui.endpoint'),
 ]).enableFor(app);
 
 new I18Next().enableFor(app);
